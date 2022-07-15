64227e80ffec9de278b751cc0c466b5dedc8ede3
Tom <tom@mycosupport.co.uk>
2022-07-15 17:22:19 +0100

    15/06/22 17:22

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/html">
   <head>
@@ -16,30 +15,39 @@
   <body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-  <div class="container-fluid">
-      <div class="row col-12" id="headerWrap">
-          <p class="col-6" id="phoneNumber">+44(0)207 183 3787</p>
-          <p class="col-6" id="offer">Want 10% off your first order? Click here</p>
+  <div class="container-fluid" id="topBanner">
+      <div class="container">
+          <div class="headerWrap">
+              <p id="phoneNumber"><img src="Images/phone.png" alt="phone handle" id="phone"> +44(0)207 183 3787</p>
+              <p id="offer">Want 10% off your first order? Click here</p>
+              <img src="Images/socialMedia.png" alt="Social Media Icons" id="socialMedia">
+          </div>
       </div>
   </div>
 
   <header class="container-fluid" id="logo">
         <div class="container">
-            <img class="row" id="mycoLogo" src="Images/myco-works-logo.svg" alt="MyCo Works Logo" width="300px" height="72px">
+            <div class="clientLogin">
+                <img class="row" id="mycoLogo" src="Images/myco-works-logo.svg" alt="MyCo Works Logo" width="300px" height="72px">
+            </div>
         </div>
   </header>
 
   <nav class="container">
-          <p>Address Services</p>
-          <p>Company Formations</p>
-          <p>Pricing</p>
-          <p>Other Services</p>
-          <p>Locations</p>
-          <p>FAQs</p>
-          <p>About Us</p>
-          <p>Blogs</p>
-          <p>Partners</p>
-          <p>Contact Us</p>
+      <div class="navigation">
+          <ul>
+              <li>Address Services</li>
+              <li>Company Formations</li>
+              <li>Pricing</li>
+              <li>Other Services</li>
+              <li>Locations</li>
+              <li>FAQs</li>
+              <li>About Us</li>
+              <li>Blogs</li>
+              <li>Partners</li>
+              <li>Contact Us</li>
+          </ul>
+      </div>
   </nav>
 
 
@@ -49,17 +57,20 @@
               <h1>A comprehensive range of Virtual Office services to help your business grow</h1>
               <p>Create an instant presence and protect your privacy with our choice of prestigious business addresses.
                   We'll forward your post and handle your calls! Get in touch with a member of our expert team for more information</p>
-              <img src="Images/home-banner-img.jpg" alt="A man and woman looking at a tablet"/>
+              <img src="Images/home-banner-img.jpg" alt="A man and woman looking at a tablet" id="homeBanner-img"/>
           </div>
           <div class="row">
               <button type="button" name="See Pricing" id="seePricing">See Pricing</button>
               <button type="button" name="Our Locations" id="ourLocations">Our Locations</button>
           </div>
+          <div class="trustPilot">
+              <img src="Images/trustPilot.png" alt="Our Trustpilot Score">
+          </div>
       </div>
   </section>
 
   <section class="container-fluid">
-      <div class="row">
+      <div class="container">
           <div class="middleBanner">
 
               <!--Trusted Clients-->
@@ -68,7 +79,7 @@
                       <img src="Images/Trusted_Clients.svg" alt="Three stick figures" id="clients"/>
                   </div>
                   <div class="trustedClients-text">
-                      <h2>30K+</h2>
+                      <h3>30K+</h3>
                       <p>Trusted Clients</p>
                   </div>
               </div>
@@ -79,7 +90,7 @@
                       <img src="Images/Years_of_Experience_icon.svg" alt="A calendar with a tick" id="over100Years">
                   </div>
                   <div class="experience-text">
-                      <h2>Over 100 Years</h2>
+                      <h3>Over 100 Years</h3>
                       <p>Combined Industry Experience</p>
                   </div>
               </div>
@@ -90,7 +101,7 @@
                       <img src="Images/Amazing_Offices_icon.svg" alt="Small building" id="offices">
                   </div>
                   <div class="prestigiousOffices-text">
-                      <h2>6</h2>
+                      <h3>6</h3>
                       <p>Prestigious Offices</p>
                   </div>
               </div>
@@ -98,10 +109,10 @@
               <!--Live Chat-->
               <div class="liveChat">
                   <div class="liveChat-img">
-
+                    <img src="Images/Live%20Chat.png" alt="Speech Bubbles" id="chat">
                   </div>
                   <div class="liveChat-text">
-                      <h2>Live Chat</h2>
+                      <h3>Live Chat</h3>
                       <p>Chat with one of our friendly staff</p>
                   </div>
               </div>
