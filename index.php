35b45fb8aa99d43daadf0e9c197526c6bbfb6c4a
Tom <tom@mycosupport.co.uk>
2022-07-18 12:23:39 +0100

    18/07/22 12:22

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   <div class="container-fluid" id="topBanner">
-      <div class="container">
+      <div class="container" id="topHeader">
           <div class="headerWrap">
-              <p id="phoneNumber"><img src="Images/phone.png" alt="phone handle" id="phone"> +44(0)207 183 3787</p>
+              <p id="phoneNumber"><img src="Images/phone.png" alt="phone handle" id="phone"> +44 (0) 207 183 3787</p>
               <p id="offer">Want 10% off your first order? Click here</p>
               <img src="Images/socialMedia.png" alt="Social Media Icons" id="socialMedia">
           </div>
@@ -53,21 +53,37 @@
 
   <section class="jumbotron">
       <div class="container">
-          <div class="row text-left">
-              <h1>A comprehensive range of Virtual Office services to help your business grow</h1>
-              <p>Create an instant presence and protect your privacy with our choice of prestigious business addresses.
-                  We'll forward your post and handle your calls! Get in touch with a member of our expert team for more information</p>
-              <img src="Images/home-banner-img.jpg" alt="A man and woman looking at a tablet" id="homeBanner-img"/>
-          </div>
           <div class="row">
-              <button type="button" name="See Pricing" id="seePricing">See Pricing</button>
-              <button type="button" name="Our Locations" id="ourLocations">Our Locations</button>
+              <div class="col-7">
+                  <h1>
+                      A comprehensive range of
+                      <br>
+                      Virtual Office services
+                      <br>to help your business grow
+                  </h1>
+                  <p class="frontPageText">Create an instant presence and protect your privacy with our choice of prestigious business addresses.
+                      We'll forward your post and handle your calls! Get in touch with a member of our expert team for more information</p>
+                  <div class="viewButtons">
+                      <button type="button" name="See Pricing" id="viewPricing">View Pricing</button>
+                      <button type="button" name="Our Locations" id="ourLocations">Our Locations</button>
+                  </div>
+              </div>
+
+
+              <div class="col-5">
+                  <div class="addressImage">
+                      <img src="Images/home-banner-img.jpg" alt="A man and woman looking at a tablet" id="homeBanner-img"/>
+                  </div>
+              </div>
           </div>
+
           <div class="trustPilot">
               <img src="Images/trustPilot.png" alt="Our Trustpilot Score">
           </div>
+
       </div>
   </section>
+
 
   <section class="container-fluid">
       <div class="container">
@@ -120,6 +136,33 @@
       </div>
   </section>
 
+  <section class="aboutUsWrapper">
+      <div class="container">
+          <div class="row" id="aboutRow">
+              <div class="col-12 col-xl-6">
+                  <div class="aboutImage">
+                      <img src="Images/about-us-img.png" alt="group of people in background a man asking for a handshake in the foreground"/>
+                  </div>
+              </div>
+              <div class="col-12 col-xl-6">
+                  <div class="aboutUs">
+                      <h3 class="introTitle">Introducing MYCO Works</h3>
+                      <p class="introText">
+                          MYCO Works provides a range of affordable Virtual Office packages to allow you more free time,
+                          so you can focus on growing your business. If you're looking for a prestigious business address,
+                          mail handling or call answering services, we offer a number of packages to suit your business needs.
+                          If you're unsure which package you need? contact our friendly team for advice.
+                      </p>
+                      <p class="introText">
+                          Sign up today and start using your new real business address in minutes, with access to meeting rooms whenever you need it.
+                          Stand out from your competitors and promote your new business address on your website, invoices, marketing materials and more.
+                      </p>
+                      <button type="button" name="About Us" id="aboutUsButton">About Us</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
   </body>
 </html>
 
