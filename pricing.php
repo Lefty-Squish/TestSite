40136ba102f379e4eda3773a48f91bc7e2baa737
Tom <tom@mycosupport.co.uk>
2022-09-06 17:26:07 +0100

    06/09/22 17:25

--- pricing.php
+++ pricing.php
@@ -72,7 +72,7 @@
         <?php include 'Tables/Table1.php';?>
 
 <!--        Unsure Diagram  -->
-        <section id="unsureChart">
+        <section id="unsureChart" class="py-md-5 mb-5">
             <div class="container">
                 <div class="row">
                     <div class="col-12 pt-5">
